a4ae785d3b22f1fc4adda8ddd4fe7c40104f62fb
phdfar <iplserver01@gmail.com>
2023-05-10 20:44:28 +0430

    add pth

--- run_train.py
+++ run_train.py
@@ -117,7 +117,7 @@ def start(args):
   
   if args.mode=='test':
     test_gen = dispatcher_loader[args.branch_input](args,allframe_test)
-    if args.loss='FOCAL':
+    if args.loss=='FOCAL':
       mymodel = load_model('my_model.h5', custom_objects={'focal_loss': focal_loss()})
     else:
       mymodel = load_model(args.model_dir)
